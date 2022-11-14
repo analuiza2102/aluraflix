ce1e0e492454e881329fc696c112be15f60a683e
unknown <aninhaguiiimaraes2102@gmail.com>
2022-11-14 12:25:22 -0300

    commit flix

---
+++ README.md
@@ -0,0 +1,4 @@
+# AluraFlix v1- ID3
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/analuiza2102/pen/ZEoBGPN](https://codepen.io/analuiza2102/pen/ZEoBGPN).
+
---
+++ dist/aluraflix.js
@@ -0,0 +1,14 @@
+var listaFilmes = ["clube da luta, poderoso chefao,simplesmente acontece"];
+
+function adicionarFilme() {
+    var filmeFavorito = document.getElementById("filme").value;
+    if (filmeFavorito.endsWith(".jpg")) {
+        listaFilmes.push(filmeFavorito);
+        document.getElementById("listaFilmes").innerHTML = listaFilmes;
+ 
+
+document.write(<p> +filme1 + </p>);
+document.write(<p> +filme2 + </p>);
+document.write(<p> +filme3 + </p>);
+
+}
